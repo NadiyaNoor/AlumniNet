ac44f91eded65ea602bc30029e20e9b7161dc5e0
Jorge Quezada <quezada.jorge47@gmail.com>
2020-04-16 19:58:49 -0400

    Add files via upload
    updated pulls from database

--- Application/app/src/main/java/com/example/alumninet/ui/myProfile/MyProfileFragment.java
+++ Application/app/src/main/java/com/example/alumninet/ui/myProfile/MyProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.alumninet.ui.myProfile;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +16,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.alumninet.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class MyProfileFragment extends Fragment {
 
@@ -23,6 +30,9 @@ public class MyProfileFragment extends Fragment {
         myProfileViewModel =
                 ViewModelProviders.of(this).get(MyProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_my_profile, container, false);
+        TextView userName = root.findViewById(R.id.userName);
+        TextView userEmail = root.findViewById(R.id.userEmail);
+        ImageView userImage = root.findViewById(R.id.userImage);
         /*final TextView textView = root.findViewById(R.id.text_my_profile);
         myProfileViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +40,30 @@ public class MyProfileFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+            //Get userName to user profile...
+            if(email==null){
+                email = "Wrong email bub";
+                userName.setText(email);
+            }
+            //userName.setText(name); Add after initialized
+            userEmail.setText(email);
+            //userImage.setImageURI(photoUrl);
+
+        }
         return root;
     }
 }
