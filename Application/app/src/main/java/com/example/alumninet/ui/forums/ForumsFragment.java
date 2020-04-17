b36fc33ed1c80aa233e6048a0257fbefac2c3ae3
Nadiya Noor <32606236+NadiyaNoor@users.noreply.github.com>
2020-04-17 15:25:42 -0400

    Adding forum posting functionality

--- Application/app/src/main/java/com/example/alumninet/ui/forums/ForumsFragment.java
+++ Application/app/src/main/java/com/example/alumninet/ui/forums/ForumsFragment.java
@@ -1,11 +1,17 @@
 package com.example.alumninet.ui.forums;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,7 +22,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.alumninet.ForumListAdapter;
 import com.example.alumninet.Forums;
+import com.example.alumninet.ForumsActivity;
 import com.example.alumninet.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,12 +33,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ForumsFragment extends Fragment {
 
     ListView listView;
-    DatabaseReference databaseReference;
-    ArrayList<String> forumList = new ArrayList<>();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Forums");;
+    ArrayList<String> titleList = new ArrayList<>();
     ArrayList<String> descriptionList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
 
@@ -42,9 +52,9 @@ public class ForumsFragment extends Fragment {
         //TextView title = root.findViewById(R.id.textView);
         //TextView description = root.findViewById(R.id.textView2);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("Forums");
+        //databaseReference = FirebaseDatabase.getInstance().getReference("Forums");
         listView = root.findViewById(R.id.forumList);
-        arrayAdapter = new ForumListAdapter(getActivity(), forumList, descriptionList);
+        arrayAdapter = new ForumListAdapter(getActivity(), titleList, descriptionList);
         listView.setAdapter(arrayAdapter);
 
         //title.setText();
@@ -55,14 +65,25 @@ public class ForumsFragment extends Fragment {
                 String title = dataSnapshot.getValue(Forums.class).getTitle();
                 String description = dataSnapshot.getValue(Forums.class).getDescription();
                 //String description = dataSnapshot.getValue(Forums.class).toString();
-                forumList.add(title);
+                titleList.add(title);
                 descriptionList.add(description);
+                Collections.reverse(titleList);
+                Collections.reverse(descriptionList);
                 arrayAdapter.notifyDataSetChanged();
+
+
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
+                String title = dataSnapshot.getValue(Forums.class).getTitle();
+                String description = dataSnapshot.getValue(Forums.class).getDescription();
+                //String description = dataSnapshot.getValue(Forums.class).toString();
+                titleList.add(title);
+                descriptionList.add(description);
+                Collections.reverse(titleList);
+                Collections.reverse(descriptionList);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -87,12 +108,88 @@ public class ForumsFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                Toast.makeText(getContext(),
+                /*Toast.makeText(getContext(),
                         "You Clicked: " +  listView.getItemAtPosition(position), Toast.LENGTH_LONG)
+                        .show();*/
+                Toast.makeText(getContext(),
+                        "You Clicked: " + titleList.get(position) , Toast.LENGTH_LONG)
                         .show();
+
+                Intent intent = new Intent(getActivity(), ForumsActivity.class);
+
+                intent.putExtra("Title", titleList.get(position));
+                intent.putExtra("Description", descriptionList.get(position));
+                startActivity(intent);
 
             }
         });
+
+
+        FloatingActionButton floatingActionButton = root.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Create New Forum");
+                // Set up the input
+                // Add a TextView here for the "Title" label, as noted in the comments
+                LinearLayout layout = new LinearLayout(getActivity());
+                layout.setOrientation(LinearLayout.VERTICAL);
+                final EditText title = new EditText(getActivity());
+                title.setHint("Title");
+                layout.addView(title); // Notice this is an add method
+
+                // Add another TextView here for the "Description" label
+                final EditText description = new EditText(getActivity());
+                description.setHint("Description");
+                layout.addView(description); // Another add method
+
+                builder.setView(layout);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Forums forum = new Forums(title.getText().toString(),
+                                description.getText().toString());
+                        addForum(forum);
+
+                        }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+            }
+        });
+
+
         return root;
+    }
+
+
+    private void addForum(Forums forum) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Forums").push();
+
+        // get post unique ID and update post key
+        String key = myRef.getKey();
+        forum.setPostKey(key);
+
+
+        // add post data to firebase database
+        myRef.setValue(forum).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getContext(),
+                        "Posted Successfully ", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
