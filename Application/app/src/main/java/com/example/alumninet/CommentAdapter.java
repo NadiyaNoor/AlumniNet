407b925585e8a4ec664e8e13e329a3c86256c65d
Nadiya Noor <32606236+NadiyaNoor@users.noreply.github.com>
2020-04-17 22:22:38 -0400

    Adding extra info to forum

---
+++ Application/app/src/main/java/com/example/alumninet/CommentAdapter.java
@@ -0,0 +1,73 @@
+package com.example.alumninet;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
+
+    private Context mContext;
+    private List<Comment> mData;
+
+
+    public CommentAdapter(Context mContext, List<Comment> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(mContext).inflate(R.layout.activity_comment_item,parent,false);
+        return new CommentViewHolder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+
+        holder.tv_name.setText(mData.get(position).getUname());
+        holder.tv_content.setText(mData.get(position).getContent());
+        holder.tv_date.setText(timestampToString((Long)mData.get(position).getTimestamp()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class CommentViewHolder extends RecyclerView.ViewHolder{
+        TextView tv_name,tv_content,tv_date;
+
+        public CommentViewHolder(View itemView) {
+            super(itemView);
+            tv_name = itemView.findViewById(R.id.comment_username);
+            tv_content = itemView.findViewById(R.id.comment_content);
+            tv_date = itemView.findViewById(R.id.comment_date);
+        }
+    }
+
+
+
+    private String timestampToString(long time) {
+
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(time);
+        String date = DateFormat.format("hh:mm",calendar).toString();
+        return date;
+
+
+    }
+
+
+}
--- Application/app/src/main/java/com/example/alumninet/Forums.java
+++ Application/app/src/main/java/com/example/alumninet/Forums.java
@@ -2,26 +2,36 @@ package com.example.alumninet;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.database.ServerValue;
+
+import java.util.Date;
+
 public class Forums {
     String title;
     String description;
     String forumKey;
+    String username;
+    String date ;
+    Date currDate = new Date();
+
+
 
     public Forums(){
     }
 
-    public Forums(String title, String description){//, String forumKey){
+    public Forums(String title, String description, String username){
         this.title = title;
         this.description = description;
-        //this.forumKey= forumKey;
+        this.username = username;
+        //this.userId = this.userId;
     }
 
-    public String getPostKey() {
+    public String getForumKey() {
         return forumKey;
     }
 
-    public void setPostKey(String postKey) {
-        this.forumKey = postKey;
+    public void setForumKey(String forumKey) {
+        this.forumKey = forumKey;
     }
 
     public String getTitle() {
@@ -40,9 +50,27 @@ public class Forums {
         this.description = description;
     }
 
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String date) {
+        //date = currDate.toString();
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     @NonNull
     @Override
     public String toString() {
         return title + " \n \t " + description;
     }
+
 }
